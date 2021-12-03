721920b65a6ef706800e4523c5ebcde84fd37892
Lorenzo <lor.epifani@stud.uniroma3.it>
2021-12-03 18:36:17 +0100

    add kata comments

--- js/6-kyu/toCamelCase.js
+++ js/6-kyu/toCamelCase.js
@@ -1,3 +1,11 @@
+/* Complete the method/function so that it converts dash/underscore 
+ * delimited words into camel casing. The first word within the output 
+ * should be capitalized only if the original word was capitalized 
+ * (known as Upper Camel Case, also often referred to as Pascal case). */
+
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
 function toCamelCase(str){
     let arr = str.split(/-|_/g);
     for(let i = 1; i < arr.length; i++) {
