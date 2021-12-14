c70ee4b815d9e1fcc8b8fbb661704627fd4d4f6e
Lorenzo <lor.epifani@stud.uniroma3.it>
2021-12-14 21:49:01 +0100

    pi_approx kata

---
+++ ocaml/6-kyu/pi_approx.ml
@@ -0,0 +1,16 @@
+open Batteries
+
+let pi = 4.0 *. (atan 1.0)
+let rnd10 x = 
+  (x *. 1E10 |> int_of_float |> float_of_int) /. 1E10
+
+let iter_pi epsilon =
+    let rec aux iter appr =
+        let pp = rnd10 (appr *. 4.) in
+        if Float.abs(pp -. pi) < epsilon then (iter, pp)
+        else 
+            aux (iter + 1) 
+            (((Float.pow (-1.) (float_of_int(iter))/.(float_of_int((2*iter)+1)))
+            +. appr))
+    in aux 0 0.
+        
